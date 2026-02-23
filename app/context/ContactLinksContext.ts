13b53eedde7c4be3e4cf9d97c8cdd051af79b2b1
Marcos Evaristo <marcosh.evaristo15@gmail.com>
2026-02-22 23:34:06 -0300

    Contact links. Added context to keep contact data for reusability.

---
+++ app/context/ContactLinksContext.ts
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export interface IContactLinksContext {
+  linkedin: string | null;
+  whatsapp: string | null;
+  email: string | null;
+}
+
+export const ContactLinksContext = createContext<IContactLinksContext>({
+  linkedin: null,
+  whatsapp: null,
+  email: null
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -5,10 +7,11 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import { ContactLinksContext } from "./context/ContactLinksContext";
 
 export default function Home() {
   return (
-    <>
+    <ContactLinksContext.Provider value={{ linkedin: "https://www.linkedin.com/in/marcos-evaristo-a2497393/", whatsapp: "https://wa.me/+55999400403", email: "marcosh.evaristo15@gmail.com" }}>
       <Header />
       <main>
         <Hero />
@@ -18,6 +21,6 @@ export default function Home() {
         <Contact />
       </main>
       <Footer />
-    </>
+    </ContactLinksContext.Provider>
   );
 }
